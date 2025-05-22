0b07cc8567a42135610a06391053477cb15ef07c
Roman <roman.petrushenko1937@gmail.com>
2025-05-22 20:35:11 +0300

    Fix base path and homepage for GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx' // Імпортуємо AuthProvider
 import './index.css'
 
+const repoName = import.meta.env.MODE === 'production' ? '/webmeteo' : '';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={repoName}>
       <AuthProvider> {/* Обгортаємо App */}
         <App />
       </AuthProvider>
